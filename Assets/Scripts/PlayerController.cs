5cbafa63b483b9075ab878dbfa9f3604b8864c53
iTzGooDLife <julio.cerdav@sansano.usm.cl>
2020-10-10 15:57:16 -0300

    tarea

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movimientoHorizontal, 0.0f, movimientoVertical);
+
+        rb.AddForce (movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Contador: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "¡Ganaste!";
+        }
+    }
+}
